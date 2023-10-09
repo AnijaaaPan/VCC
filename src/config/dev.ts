7ee7db71c7d039a1d9020ab633a0501914a95771
The.First.Step <46297678+AnijaaaPan@users.noreply.github.com>
2023-10-10 01:46:09 +0900

    fix(config): 不要なコードを削除

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -2,6 +2,5 @@ export default {
   discord: {
     defaultLogChannelId: '1124606719599845479',
     errorMentionRoleId: '',
-  },
-  redisOptions: {},
+  }
 }
--- src/config/prod.ts
+++ src/config/prod.ts
@@ -2,10 +2,5 @@ export default {
   discord: {
     defaultLogChannelId: '1124606719599845478',
     errorMentionRoleId: '1124606717687242763',
-  },
-  redisOptions: {
-    tls: {
-      rejectUnauthorized: false,
-    },
-  },
+  }
 }
